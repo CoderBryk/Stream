f9bc6e4cc8dcb773d2180b0236482240323cf924
BlossomEze <blossomeze0@gmail.com>
2023-01-27 20:33:30 +0100

    main

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Box, Typography, Stack } from '@mui/material'
-import Sidebar from './Sidebar'
+import { Sidebar, Videos } from './index'
 
 function Feed() {
   return (
@@ -10,6 +10,12 @@ function Feed() {
         <Typography className='copyright' variant='body2' sx={{mt: 2.5, color: '#fff' }}>
         copyright 2023 Blossom Eze
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
+          New<span style={{ color: '#F31503'}}>Videos</span>
+        </Typography>
+        <Videos />
       </Box>
     </Stack>
   )
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -9,8 +9,8 @@ function Sidebar() {
     <Stack direction='row' sx={{overflowY: 'auto', height: {sx: 'auto', md: '92%'}, flexDirection: {md:'column'}, }}>
        { categories.map((category) => (
         <button className='sidebar_btn' style={{background: category.name === selectedCategory && '#FC1503', color: '#fff'}} key={ category.name }>
-            <span>{ category.icon }</span>
-            <span>{ category.name }</span>
+            <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>{ category.icon }</span>
+            <span style={{ opacity: category.name === selectedCategory ? '1' : '0.7' }}>{ category.name }</span>
         </button>
        )) }
     </Stack>
