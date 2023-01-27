f412705d4ece155c5a18cbff1a139512a958a0b9
BlossomEze <blossomeze0@gmail.com>
2023-01-27 22:22:42 +0100

    main

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -8,19 +8,19 @@ function Feed() {
 
     useEffect(() => {
     fetch(`search?part=snippet&q=${selectedCategory}`);
-  }, []);
+  }, [selectedCategory]);
 
   return (
     <Stack sx={{flexDirection: {sx: 'column', md: 'row'} }}>
       <Box sx={{height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', padding: {sx: 0, md: 2}}}>
-        <Sidebar />
+        <Sidebar selectedCategory = { selectedCategory } setSelectedCategory= { setSelectedCategory } />
         <Typography className='copyright' variant='body2' sx={{mt: 2.5, color: '#fff' }}>
         copyright 2023 Blossom Eze
         </Typography>
       </Box>
       <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
         <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
-          New<span style={{ color: '#F31503'}}>Videos</span>
+          {selectedCategory}<span style={{ color: '#F31503', paddingLeft: 8 }}>Videos</span>
         </Typography>
         <Videos />
       </Box>
